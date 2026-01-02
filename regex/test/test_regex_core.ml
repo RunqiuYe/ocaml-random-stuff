a22671c2371410b89bba7cffa292207788b66a45
Runqiu Ye <runqiuy@andrew.cmu.edu>
2026-01-02 00:19:19 -0500

    add one testcase

--- regex/test/test_regex_core.ml
+++ regex/test/test_regex_core.ml
@@ -80,3 +80,46 @@ let () =
   assert (Nfa.is_match r "aaaaaaaaaaaaaaaa");
   assert (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
   assert (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaa")
+
+let () =
+  let m = Maybe (Char 'a') in
+  let a = Char 'a' in
+  let nm =
+    Concat
+      ( Concat
+          ( Concat
+              ( Concat (Concat (m, m), Concat (m, m)),
+                Concat (Concat (m, m), Concat (m, m)) ),
+            Concat
+              ( Concat (Concat (m, m), Concat (m, m)),
+                Concat (Concat (m, m), Concat (m, m)) ) ),
+        Concat
+          ( Concat
+              ( Concat (Concat (m, m), Concat (m, m)),
+                Concat (Concat (m, m), Concat (m, m)) ),
+            Concat
+              ( Concat (Concat (m, m), Concat (m, m)),
+                Concat (Concat (m, m), Concat (m, m)) ) ) )
+  in
+  let na =
+    Concat
+      ( Concat
+          ( Concat
+              ( Concat (Concat (a, a), Concat (a, a)),
+                Concat (Concat (a, a), Concat (a, a)) ),
+            Concat
+              ( Concat (Concat (a, a), Concat (a, a)),
+                Concat (Concat (a, a), Concat (a, a)) ) ),
+        Concat
+          ( Concat
+              ( Concat (Concat (a, a), Concat (a, a)),
+                Concat (Concat (a, a), Concat (a, a)) ),
+            Concat
+              ( Concat (Concat (a, a), Concat (a, a)),
+                Concat (Concat (a, a), Concat (a, a)) ) ) )
+  in
+  let r = Concat (nm, na) in
+  assert (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  assert (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  assert (not (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaa"));
+  assert (Nfa.is_match r "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
