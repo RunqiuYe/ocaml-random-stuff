67435bbe082931b32add8315eeeea39e137eb8db
Runqiu Ye <runqiuy@andrew.cmu.edu>
2026-05-31 17:53:29 -0400

    reformat

--- promise/promise.ml
+++ promise/promise.ml
@@ -1,57 +1,67 @@
 (** A signature for Lwt-style promises, with better names *)
 module type Promise = sig
-  type 'a state = Pending | Resolved of 'a | Rejected of exn
+  type 'a state =
+    | Pending
+    | Resolved of 'a
+    | Rejected of exn
+
   type 'a promise
   type 'a resolver
 
-  val make : unit -> 'a promise * 'a resolver
   (** [make ()] is a new promise and resolver. The promise is pending. *)
+  val make : unit -> 'a promise * 'a resolver
 
-  val return : 'a -> 'a promise
   (** [return x] is a new promise that is already resolved with value [x]. *)
+  val return : 'a -> 'a promise
 
-  val state : 'a promise -> 'a state
   (** [state p] is the state of the promise *)
+  val state : 'a promise -> 'a state
 
-  val resolve : 'a resolver -> 'a -> unit
   (** [resolve r x] resolves the promise [p] associated with [r] with value [x],
       meaning that [state p] will become [Resolved x]. Requires: [p] is pending.
   *)
+  val resolve : 'a resolver -> 'a -> unit
 
-  val reject : 'a resolver -> exn -> unit
   (** [reject r x] rejects the promise [p] associated with [r] with exception
       [x], meaning that [state p] will become [Rejected x]. Requires: [p] is
       pending. *)
+  val reject : 'a resolver -> exn -> unit
 
-  val ( >>= ) : 'a promise -> ('a -> 'b promise) -> 'b promise
   (** [p >>= c] registers callback [c] with promise [p]. When the promise is
       resolved, the callback will be run on the promises's contents. If the
       promise is never resolved, the callback will never run. *)
+  val ( >>= ) : 'a promise -> ('a -> 'b promise) -> 'b promise
 end
 
 module MyPromise : Promise = struct
-  type 'a state = Pending | Resolved of 'a | Rejected of exn
+  type 'a state =
+    | Pending
+    | Resolved of 'a
+    | Rejected of exn
 
-  type 'a handler = 'a state -> unit
   (** RI: the input may not be [Pending] *)
+  type 'a handler = 'a state -> unit
 
-  type 'a promise = {
-    mutable state : 'a state;
-    mutable handlers : 'a handler list;
-  }
   (** RI: if [state <> Pending] then [handlers = []]. *)
+  type 'a promise =
+    { mutable state : 'a state
+    ; mutable handlers : 'a handler list
+    }
 
   type 'a resolver = 'a promise
 
   let enqueue (handler : 'a handler) (promise : 'a promise) : unit =
     promise.handlers <- handler :: promise.handlers
+  ;;
 
   let write_once p s =
     if p.state = Pending then p.state <- s else invalid_arg "cannot write twice"
+  ;;
 
   let make () =
     let p = { state = Pending; handlers = [] } in
-    (p, p)
+    p, p
+  ;;
 
   let return x = { state = Resolved x; handlers = [] }
   let state p = p.state
@@ -63,6 +73,7 @@ module MyPromise : Promise = struct
     r.handlers <- [];
     write_once r st;
     List.iter (fun f -> f st) handlers
+  ;;
 
   let resolve r x = resolve_or_reject r (Resolved x)
   let reject r x = resolve_or_reject r (Rejected x)
@@ -71,25 +82,28 @@ module MyPromise : Promise = struct
     | Pending -> failwith "handler RI violated"
     | Rejected exc -> reject resolver exc
     | Resolved x -> resolve resolver x
+  ;;
 
-  let handler_of_callback (callback : 'a -> 'b promise)
-      (output_resolver : 'b resolver) : 'a handler = function
+  let handler_of_callback (callback : 'a -> 'b promise) (output_resolver : 'b resolver)
+    : 'a handler
+    = function
     | Pending -> failwith "hanlder RI violated"
     | Rejected exc -> reject output_resolver exc
-    | Resolved x -> (
-        let promise = callback x in
-        match promise.state with
-        | Rejected exc -> reject output_resolver exc
-        | Resolved y -> resolve output_resolver y
-        | Pending -> enqueue (handler output_resolver) promise)
+    | Resolved x ->
+      let promise = callback x in
+      (match promise.state with
+       | Rejected exc -> reject output_resolver exc
+       | Resolved y -> resolve output_resolver y
+       | Pending -> enqueue (handler output_resolver) promise)
+  ;;
 
-  let ( >>= ) (input_promise : 'a promise) (callback : 'a -> 'b promise) :
-      'b promise =
+  let ( >>= ) (input_promise : 'a promise) (callback : 'a -> 'b promise) : 'b promise =
     match input_promise.state with
     | Resolved x -> callback x
     | Rejected exc -> { state = Rejected exc; handlers = [] }
     | Pending ->
-        let output_promise, output_resolver = make () in
-        enqueue (handler_of_callback callback output_resolver) input_promise;
-        output_promise
+      let output_promise, output_resolver = make () in
+      enqueue (handler_of_callback callback output_resolver) input_promise;
+      output_promise
+  ;;
 end
